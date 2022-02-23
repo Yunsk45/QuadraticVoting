73d3914b6810de957410f9e863fa861a7c664efb
Yunsk45 <nisszer@gmail.com>
2022-02-23 12:23:46 +0100

    Implementing auxiliary functions and function to create an Item

--- contracts/QuadraticVoting.sol
+++ contracts/QuadraticVoting.sol
@@ -14,4 +14,42 @@ contract QuadraticVoting {
       uint totalNegativeWeight;
    }
 
+   uint constant public voteCost = 10_000_000_000;
+   mapping(uint => Item) public items;
+   uint public itemCount = 0;
+
+   event ItemCrated(uint itemId);
+   event Voted(uint itemId, uint weight, bool positive);
+
+   function currenctWeight(uint itemId, address addr, bool isPositive) public view returns(uint) {
+      if(isPositive) {
+         return items[itemId].positiveVotes[addr];
+      }
+      else {
+         return items[itemId].negativeVotes[addr];
+      }
+   }
+
+   function calcCost(uint currWeight, uint weight) public pure returns(uint) {
+      if(currWeight > weight) {
+         return weight * weight * voteCost;
+      } else if (currWeight < weight) {
+         return (weight*weight - currWeight*currWeight) * voteCost;
+      }
+      else {
+         return 0;
+      }
+   }
+
+   function createItem(bytes32 title, string memory imageHash, string memory description) public {
+      uint itemId = itemCount;
+      itemCount++; //Clearer separation of incrementation
+      Item storage item = items[itemId];
+      item.owner = msg.sender;
+      item.title = title;
+      item.imageHash = imageHash;
+      item.description = description;
+      emit ItemCrated(itemID);
+   }
 }
+
